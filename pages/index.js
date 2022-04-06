26d2431fe36dd19d7f7aaa607cd0a39e884c4174
Connor Mackay <connor.mackay@gmail.com>
2022-04-06 13:23:17 -0400

    Add error handling to index.js

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,23 @@ function HomePage(props) {
 };
 
 export async function getStaticProps(context) {
-  
+
   console.log("regenerating!!!");
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFileSync(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return { 
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  };
+
+  if (data.products.length === 0) {
+    return { notFound: true }
+  }
   return {
     props: {
       products: data.products
