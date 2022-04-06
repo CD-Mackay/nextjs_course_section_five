623c9ee38b1b5610c2ca794bc7d92112c49c86a5
Connor Mackay <connor.mackay@gmail.com>
2022-04-06 14:11:33 -0400

    Add getstaticpaths

--- pages/[productId].js
+++ pages/[productId].js
@@ -1,31 +1,41 @@
-import { Fragment } from 'react';
-import fs from 'fs';
-import path from 'path'; 
+import { Fragment } from "react";
+import fs from "fs";
+import path from "path";
 
-function ProductPage({loadedProduct}) {
-
+function ProductPage({ loadedProduct }) {
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
       <p>{loadedProduct.description}</p>
     </Fragment>
-  )
-};
+  );
+}
 
 export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.productId;
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFileSync(filePath);
   const data = JSON.parse(jsonData);
 
-  const product = data.products.find(product => product.id === productId);
+  const product = data.products.find((product) => product.id === productId);
 
   return {
     props: {
-      loadedProduct: product
-    }
-  }
-};
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { productId: "p1" } },
+      { params: { productId: "p2" } },
+      { params: { productId: "p3" } }
+    ],
+    fallback: false,
+  };
+}
 
 export default ProductPage;
