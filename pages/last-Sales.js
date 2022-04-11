33b5323131686c53245b535e4a12ad00efb00b0a
Connor Mackay <connor.mackay@gmail.com>
2022-04-11 11:45:21 -0400

    Add lastsales page

---
+++ pages/last-Sales.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+function LastSales() {
+
+  const [sales, setSales] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch('https://nextjs-section5-e6020-default-rtdb.firebaseio.com/sales.json' )
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     const finalSales = [];
+  //     for (const key in data) {
+  //       finalSales.push({
+  //         id: key,
+  //         username: data[key].username,
+  //         volume: data[key].volume
+  //       })
+  //     }
+  //     setSales(finalSales);
+  //     setIsLoading(false);
+  //   })
+  // }, []);
+
+  if (isLoading) {
+    return <p>...loading</p>
+  }
+
+  if (!sales) {
+    return <p>no sales</p>
+  }
+
+  return (
+    <ul>
+      {/* {sales.map((element) => {
+        <li key={element.id}>
+          {element.username} --- ${element.volume}
+          </li>
+      })} */}
+      <li>I am LastSales!</li>
+    </ul>
+  )
+};
+
+export default LastSales;
