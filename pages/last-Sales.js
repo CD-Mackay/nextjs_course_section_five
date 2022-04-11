6dbed8b7031e64379920b0c1437ac52154369560
Connor Mackay <connor.mackay@gmail.com>
2022-04-11 16:16:24 -0400

    refactor promise chain

--- pages/last-Sales.js
+++ pages/last-Sales.js
@@ -1,42 +1,66 @@
 import { useEffect, useState } from "react";
-import useSWR from 'swr';
+import useSWR from "swr";
 
-function LastSales() {
-
-  const [sales, setSales] = useState([]);
-  const { data, error } = useSWR('https://nextjs-section5-e6020-default-rtdb.firebaseio.com/sales.json', (url) => fetch(url).then(res => res.json()));
+function LastSales(props) {
+  const [sales, setSales] = useState(props.sales);
+  const { data, error } = useSWR(
+    "https://nextjs-section5-e6020-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
     if (data) {
       const finalSales = [];
-          for (const key in data) {
-            finalSales.push({
-              id: key,
-              username: data[key].username,
-              volume: data[key].volume
-            })
-          };
-          setSales(finalSales);
+      for (const key in data) {
+        finalSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(finalSales);
     }
-  }, [data])
+  }, [data]);
 
   if (error) {
-    return <p>Failed to load</p>
+    return <p>Failed to load</p>;
   }
 
-  if (!data || !sales) {
-    return <p>...loading</p>
+  if (!data && !sales) {
+    return <p>...loading</p>;
   }
 
   return (
     <ul>
       {sales.map((element) => {
-       return <li key={element.id}>
-          {element.username} --- ${element.volume}
+        return (
+          <li key={element.id}>
+            {element.username} --- ${element.volume}
           </li>
+        );
       })}
     </ul>
-  )
-};
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://nextjs-section5-e6020-default-rtdb.firebaseio.com/sales.json")
+  const data = await response.json();
+
+      const finalSales = [];
+
+      for (const key in data) {
+        finalSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      return {
+        props: {
+          sales: finalSales,
+        }
+      };
+}
 
 export default LastSales;
